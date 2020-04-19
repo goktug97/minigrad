7a0b9ebc7afc91eb058f31d55b02b309a68a2c10
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2020-04-19 21:27:23 +0300

    add function with backward

--- test.py
+++ test.py
@@ -4,7 +4,17 @@ import engine
 p1 = engine.Value(20.0)
 p2 = engine.Value(20.0)
 value = engine.Value(20.0, (p1,p2))
-print(value._prev)
+# print(p1._prev)
+# print(p2._prev)
+# print(p1._prev)
+# print(p2._prev)
+# print(value._prev)
+value = p1 + p2
+value.grad = 30
+# print(value._prev)
+print(value._backward())
+print(p1.grad)
+print(p2.grad)
 
 # print(dir(value))
 
